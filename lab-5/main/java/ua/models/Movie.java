07fd45dcb228e5c0ae8dedab7f2e59426692e393
skrypaalina <skrypa.alina@gmail.com>
2025-11-27 22:40:46 +0200

    Delete lab-5 directory

--- lab-5/main/java/ua/models/Movie.java
+++
@@ -1,9 +0,0 @@
-package ua.models;
-public class Movie implements Comparable<Movie>{
-    private int id; private String title; private int year;
-    public Movie(int id,String t,int y){this.id=id;this.title=t;this.year=y;}
-    public int getId(){return id;} public String getTitle(){return title;} public int getYear(){return year;}
-    @Override public int compareTo(Movie o){ return Integer.compare(this.id,o.id);}
-    public static java.util.Comparator<Movie> byTitle=java.util.Comparator.comparing(Movie::getTitle);
-    public static java.util.Comparator<Movie> byYear=java.util.Comparator.comparingInt(Movie::getYear);
-}
